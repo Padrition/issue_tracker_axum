0ebf75ece4040f3dd376600b3b0bf0c962e1e0d3
Dmitrii Titarenko <xtitaren@mendelu.cz>
2025-01-04 00:19:18 +0100

    (auth)
    made db name to be pulled from env

--- auth/src/controllers/auth_routers.rs
+++ auth/src/controllers/auth_routers.rs
@@ -4,7 +4,8 @@ use mongodb::Client;
 use crate::{middlewares::auth_middleware::authorization_middleware, services::{auth_service::{refresh, sign_in}, hello_service::hello, user_service::create_user}};
 
 pub fn auth_routers(client: Client) -> Router {
-    let collection = client.database("dira").collection("users");
+    let collection = client.database(&std::env::var("DATABASE_NAME").expect("No DATABASE_NAME env present."))
+        .collection("users");
 
     Router::new()
         .route("/signin", post(sign_in))
