b8e74ffdad13e52962db6d2a3ce0ef45c8119c43
Dmitrii Titarenko <padrition@gmail.com>
2025-01-10 21:43:26 +0100

    (issue get)
    get issue from board by board id ; get issue by issue id

--- issue_api/src/controllers/issue_router.rs
+++ issue_api/src/controllers/issue_router.rs
@@ -1,8 +1,15 @@
 use auth::auth_middleware::authorization_middleware;
-use axum::{middleware, routing::post, Router};
+use axum::{
+    middleware,
+    routing::{get, post},
+    Router,
+};
 use mongodb::Client;
 
-use crate::{service::issue_service::create_issue, utils::app_state::AppState};
+use crate::{
+    service::issue_service::{create_issue, get_issue, get_issues},
+    utils::app_state::AppState,
+};
 
 pub fn issue_router(client: Client) -> Router {
     let collection_user = client
@@ -21,6 +28,20 @@ pub fn issue_router(client: Client) -> Router {
         .route(
             "/create",
             post(create_issue).layer(middleware::from_fn_with_state(
+                collection_user.clone(),
+                authorization_middleware,
+            )),
+        )
+        .route(
+            "/board/:id",
+            get(get_issues).layer(middleware::from_fn_with_state(
+                collection_user.clone(),
+                authorization_middleware,
+            )),
+        )
+        .route(
+            "/:id",
+            get(get_issue).layer(middleware::from_fn_with_state(
                 collection_user.clone(),
                 authorization_middleware,
             )),
--- issue_api/src/service/issue_service.rs
+++ issue_api/src/service/issue_service.rs
@@ -1,5 +1,11 @@
 use auth::models::user_model::User;
-use axum::{extract::State, http::StatusCode, Extension, Json};
+use axum::{
+    debug_handler,
+    extract::{Path, State},
+    http::StatusCode,
+    Extension, Json,
+};
+use futures::TryStreamExt;
 use mongodb::{
     bson::{doc, oid::ObjectId},
     results::InsertOneResult,
@@ -13,6 +19,7 @@ use crate::{
     utils::app_state::AppState,
 };
 
+#[debug_handler]
 pub async fn create_issue(
     State(state): State<AppState>,
     Extension(current_user): Extension<User>,
@@ -87,4 +94,102 @@ pub async fn create_issue(
         })?;
 
     Ok(Json(insert_result))
+}
+
+#[debug_handler]
+pub async fn get_issues(
+    State(state): State<AppState>,
+    Extension(current_user): Extension<User>,
+    Path(id): Path<String>,
+) -> Result<Json<Vec<Issue>>, IssueError> {
+    let board_collection = &state.board_collection;
+    let issue_collection = &state.issue_collection;
+
+    let board_objid = ObjectId::parse_str(id).map_err(|err| IssueError {
+        message: format!("Error parsing id to ObjectId: {err}"),
+        status_code: StatusCode::INTERNAL_SERVER_ERROR,
+    })?;
+
+    let board = board_collection
+        .find_one(doc! {"_id": board_objid})
+        .await
+        .map_err(|err| IssueError {
+            message: format!("Error finding board: {err}"),
+            status_code: StatusCode::INTERNAL_SERVER_ERROR,
+        })?
+        .ok_or(IssueError {
+            message: "Board not found".to_string(),
+            status_code: StatusCode::NOT_FOUND,
+        })?;
+
+    if !board.members.contains(&current_user.email) {
+        return Err(IssueError {
+            message: "Forbidden".to_string(),
+            status_code: StatusCode::FORBIDDEN,
+        });
+    }
+
+    let issues: Vec<Issue> = issue_collection
+        .find(doc! {"_id" : {"$in" : board.issues}})
+        .await
+        .map_err(|err| IssueError {
+            message: format!("Error finding issues : {err}"),
+            status_code: StatusCode::INTERNAL_SERVER_ERROR,
+        })?
+        .try_collect()
+        .await
+        .map_err(|err| IssueError {
+            message: format!("Error collection issues: {err}"),
+            status_code: StatusCode::INTERNAL_SERVER_ERROR,
+        })?;
+
+    Ok(Json(issues))
+}
+
+#[debug_handler]
+pub async fn get_issue(
+    State(state): State<AppState>,
+    Extension(current_user): Extension<User>,
+    Path(id): Path<String>,
+) -> Result<Json<Issue>, IssueError> {
+    let board_collection = &state.board_collection;
+    let issue_collection = &state.issue_collection;
+
+    let issue_objid = ObjectId::parse_str(id).map_err(|err| IssueError {
+        message: format!("Error parsing id to ObjectId: {err}"),
+        status_code: StatusCode::INTERNAL_SERVER_ERROR,
+    })?;
+
+    let board = board_collection
+        .find_one(doc! {"issues": issue_objid})
+        .await
+        .map_err(|err| IssueError {
+            message: format!("Error finding board: {err}"),
+            status_code: StatusCode::INTERNAL_SERVER_ERROR,
+        })?
+        .ok_or(IssueError {
+            message: "Board not found".to_string(),
+            status_code: StatusCode::NOT_FOUND,
+        })?;
+
+    if !board.members.contains(&current_user.email) {
+        return Err(IssueError {
+            message: "Forbidden".to_string(),
+            status_code: StatusCode::FORBIDDEN,
+        });
+    }
+
+    let issue = issue_collection
+        .find_one(doc! {"_id": issue_objid})
+        .await
+        .map_err(|err| IssueError {
+            message: format!("Error finding issue : {err}"),
+            status_code: StatusCode::INTERNAL_SERVER_ERROR,
+        })?
+        .ok_or(IssueError {
+            message: "Issue not found".to_string(),
+            status_code: StatusCode::NOT_FOUND,
+        })?;
+
+    Ok(Json(issue))
 }
